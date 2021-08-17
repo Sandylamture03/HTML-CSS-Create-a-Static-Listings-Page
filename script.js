872ea53b07c25b4eb525334d1878e5ad07725fa7
Sandy <sandylamture03@gmail.com>
2021-08-17 19:48:49 +0530

    all files required for this exercise

---
+++ script.js
@@ -0,0 +1,51 @@
+/*var website ="https://codingForEveryOne.com/";
+var teacherName ="sandeep lamture";
+console.log(teacherName);
+{
+    let apples =5;
+    const height = "6'0\"";
+console.log("I have",apples,"apples");
+console.log("I am",height);
+}*/
+//const name = "sandeep lamture";
+//let profession ="student";
+
+/*const twitter ="@bali LAmture";
+if(twitter=='@sandy Lamture'){
+
+console.log("the twitter handle is",twitter);
+}else{
+
+    console.log("the twitter hangle is",twitter);
+}*/
+
+/*const age = 17;
+if(age < 18){
+    console.log("IM not ALLOWED TO VOTE");
+}if(age < 21){
+    console.log("IM not allowed to drink beer");
+}else{
+    console.log("  allowed to drink beer");
+}*/
+
+
+
+
+
+
+/*const name = "appa";
+if(name == "vaishu"){
+    console.log("hello vaishu !");
+} else if (name == "sandeep"){
+    console.log("hello sandeep");
+}else{
+    console.log("hello guest");
+}*/
+
+
+
+
+
+
+
+
